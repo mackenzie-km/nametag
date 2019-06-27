5f4b8fc01dbb9fc841d934ce90b5d0f635ad40a3
github username <github email address>
2019-06-27 13:21:23 -0700

    Office hours notes updated

--- db/migrate/20190627182223_create_join_table_contacts_events.rb
+++ db/migrate/20190627182223_create_join_table_contacts_events.rb
@@ -2,8 +2,8 @@ class CreateJoinTableContactsEvents < ActiveRecord::Migration[5.2]
   def change
     create_join_table :contacts, :events do |t|
       t.index :contact_id
-      t.index :part_id
-      t.integer
+      t.index :event_id
+      t.integer :guests
     end
   end
 end
