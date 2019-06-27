b57c1c44590444bc0d25e32f7370f15541c250d1
github username <github email address>
2019-06-27 11:47:53 -0700

    Added model files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_182606) do
+ActiveRecord::Schema.define(version: 2019_06_27_184458) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
