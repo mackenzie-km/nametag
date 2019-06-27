812e5302f0ad86e07f55e5fce65f837b8acfecf8
github username <github email address>
2019-06-27 15:15:38 -0700

    updated users controller to create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to contacts_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,5 +24,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
-  end 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
