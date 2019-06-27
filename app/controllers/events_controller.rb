40edad1f00f009465a9e11ffe3bdfa2de7e0739a
github username <github email address>
2019-06-27 16:41:46 -0700

    added events controller actions based on contacts controller syntax

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,47 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
   end
 
   def destroy
-  end 
+    @event = Event.find(params[:id])
+    @event.delete
+    redirect_to events_path
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :date)
+  end
 end
