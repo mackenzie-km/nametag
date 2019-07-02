499ba0891fcc120e7d7a841ad5131c7f3dc9ff93
github username <github email address>
2019-07-02 13:48:41 -0700

    added helper methods to application controller to check current user and admin level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   def index
   end
+
+  private
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def check_admin(level)
+    if level != current_user.admin_level
+      flash[:message] = "You are not authorized to view the previous page."
+      redirect_to root_path
+    end
+  end
+
 end
