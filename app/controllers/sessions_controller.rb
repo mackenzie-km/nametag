92ec82bf8dcd3e894654d874ae9b164a39763335
github username <github email address>
2019-07-06 16:12:22 -0700

    Updating code so that google_token & google_refresh_token save in user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,6 +26,7 @@ class SessionsController < ApplicationController
   def googleAuth
     access_token = request.env["omniauth.auth"]
     @user = User.from_omniauth(access_token)
+    @user.save
     # Access_token is used to authenticate request made from the rails application to the google server
     @user.google_token = access_token.credentials.token
     # Refresh_token to request new access_token
