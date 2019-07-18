ee1796e3d5adc52e9dabd267689e14d407911862
github username <github email address>
2019-07-18 14:14:36 -0700

    fixed bugs in event form so that form submits guest params and also submits contact data.

--- app/models/contacts_event.rb
+++ app/models/contacts_event.rb
@@ -1,4 +1,14 @@
 class ContactsEvent < ApplicationRecord
   belongs_to :contact
   belongs_to :event
+
+  def guests=(value)
+    self[:guests] = value
+    self.save
+  end
+
+  def guests
+    self[:guests]
+  end
+
 end
