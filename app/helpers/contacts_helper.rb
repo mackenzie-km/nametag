4177d0518046409991d3c113b5a203b1df7d3d7d
github username <github email address>
2019-07-20 22:47:05 -0700

    refactored code so that button text for nested events and card layouts would be in partials

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -6,4 +6,12 @@ module ContactsHelper
       nil
     end
   end
+
+  def button_text(params)
+    ("Create Contact For Event" if !!params[:event_id]) || "Create Contact"
+  end
+
+  def button_link(params)
+    ("/events/#{params[:event_id]}/contacts/new" if !!params[:event_id]) || new_contact_path
+  end
 end
