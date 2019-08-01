0a9623cc9914b87a47f0a9357ad48f21faa4def6
github username <github email address>
2019-08-01 10:21:56 -0700

    removed custom routes in routes.rb; updated buttons to match restful route convention

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#index"
 
+# creating model routes - note that user does not need show page
   resources :contacts
   resources :events
   resources :users, only: [:index, :new, :edit, :create, :update, :destroy]
 
+# creating nested routes for contact/:id/events & events/:id/contacts
   resources :contacts, only: [:index] do
     resources :events, only: [:index]
   end
-
   resources :events, only: [:index] do
     resources :contacts, only: [:index]
   end
 
-  get "contacts/:id/delete", to: "contacts#destroy"
-  get "events/:id/delete", to: "events#destroy"
-  get "users/:id/edit", to: "users#edit"
-
+# custom
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   get "logout", to: "sessions#destroy"
