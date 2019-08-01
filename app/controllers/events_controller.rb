f210bc03f2a5f5d9fd523a7504144493d393061f
github username <github email address>
2019-08-01 14:34:15 -0700

    updated event database to have staff count and guest count overall instead of individual guest counts. fixed controller code and show code to display tehse new columns.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,28 +3,27 @@ class EventsController < ApplicationController
   before_action :redirect_if_no_login
   before_action :access_event, only: [:show, :edit, :update, :destroy]
   before_action :find_user, only: [:create, :update]
+  before_action :user_admin_level, only: [:index, :new, :create, :edit]
 
   def index
     if params[:contact_id]
       @contact = Contact.find(params[:contact_id])
       @events = @contact.events
     else
-      @events = Event.all.access(current_user.admin_level)
+      @events = Event.access(@user_admin_level)
     end
   end
 
   def new
     @event = Event.new
-    @admin = current_user.admin_level
   end
 
   def create
     date = Event.collect_date(event_params)
-    @event = Event.new(name: event_params[:name], date: date)
+    @event = Event.new(name: event_params[:name], date: date, guest_count: event_params[:guest_count], staff_count: event_params[:staff_count])
+    @event.admin_level = @user_admin_level
     if @event.save
       Contact.add_contacts(event_params, @event, @user)
-      @event.update_guests(event_params, @event)
-      @event.admin_level = current_user.admin_level
       redirect_to event_path(@event)
     else
       render :new
@@ -35,13 +34,12 @@ class EventsController < ApplicationController
   end
 
   def edit
-    @admin = current_user.admin_level
   end
 
   def update
-    @event.update(name: event_params[:name], date: Event.collect_date(event_params))
+    date = Event.collect_date(event_params)
+    @event.update(name: event_params[:name], date: date, guest_count: event_params[:guest_count], staff_count: event_params[:staff_count])
     Contact.add_contacts(event_params, @event, @user)
-    @event.update_guests(event_params, @event)
     if @event.save then redirect_to event_path(@event) else render :edit end
   end
 
@@ -52,7 +50,7 @@ class EventsController < ApplicationController
 
   private
   def event_params
-    params.require(:event).permit(:name, :date, :guests => {}, contact_ids:[])
+    params.require(:event).permit(:name, :date, :guest_count, :staff_count, contact_ids:[])
   end
 
   def find_event
