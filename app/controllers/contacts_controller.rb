7049dcc4ae685cf94f9edac4a92f09d15b10d212
github username <github email address>
2019-08-01 12:37:44 -0700

    refactored contacts_controller & added nice contact_redirect feature for creating a new nested contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,7 @@ class ContactsController < ApplicationController
   before_action :access_contact, only: [:show, :edit, :update, :destroy]
   before_action :user_admin_level, only: [:new, :edit]
 
+# FIX - write a scope for only your own contacts & button for all contacts
   def index
     if params[:event_id]
       @event = Event.find(params[:event_id])
@@ -19,11 +20,13 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+# creates contact while associating contact with applicable events and user admin level
   def create
     @contact = Contact.new(contact_params)
     @contact.admin_level=(User.find(session[:user_id]))
-    if !!Event.exists?(id: params[:contact][:event_id]) then @contact.events << Event.find_by(id: params[:contact][:event_id].to_i) end
-    if @contact.save then redirect_to contact_path(@contact) else render :new end
+    @event = Event.find_by(id: params[:contact][:event_id])
+    if (!!@event && has_access?(@event.admin_level)) then @contact.events << @event end
+    if @contact.save then nested_contact_redirect(@event, @contact) else render :new end
   end
 
   def show
@@ -60,5 +63,10 @@ class ContactsController < ApplicationController
       flash[:notice] = "You cannot edit/view this contact."
       redirect_to contacts_path
     end
+  end
+
+# chooses redirect based on whether contact is nested or not 
+  def nested_contact_redirect(event, contact)
+    if !!event then redirect_to event_contacts_path(event) else redirect_to contact_path(contact) end
   end
 end
