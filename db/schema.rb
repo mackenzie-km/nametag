4d25800ccb00fb950166d9bf8481deef33ade983
github username <github email address>
2019-09-13 22:36:25 -0700

    add source column per sarah's request

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_033517) do
+ActiveRecord::Schema.define(version: 2019_09_14_053506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_08_25_033517) do
     t.boolean "unsubscribed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source"
   end
 
   create_table "contacts_events", id: false, force: :cascade do |t|
