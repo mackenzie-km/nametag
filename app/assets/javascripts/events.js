16a1188c7d2e25947204dd1a2cb677e80ee20cea
github username <github email address>
2019-09-28 12:50:17 -0700

    added class definition to javascript file to clean up code

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -5,6 +5,73 @@ $(document).ready(function() {
   attachRemoveListeners();
 });
 
+// create shadow class to help work with data
+
+class Contact {
+ constructor(data) {
+  this.id = data["id"];
+  this.name = data["name"]
+  this.email = data["email"];
+  this.major = data["major"];
+  this.events = data["events"];
+  this.first_event = data["events"][0];
+}
+};
+
+
+// helpers to grab contact information from input
+
+function grabId() {
+  let box = $('#event_contacts_name').val();
+  let id = $('#contacts_autocomplete option').filter(function() {
+      return this.value == box;
+  }).data('id');
+  return id;
+}
+
+function lookupInBox() {
+  $.get("/contacts/" + grabId() + ".json", function(data) {
+    let info = '<label class="lb-lg">More contact info</label><br>' + organizeInfo(data);
+    $('#more-div').html(info);
+  });
+}
+
+// functions for returning html and css
+
+function organizeInfo(data) {
+  debugger
+  if (!!data["id"]) {
+    let contact = new Contact(data)
+    for (let [key, value] of Object.entries(contact)) {
+      return `<b>${key}</b>: ${value}<br>`
+    }
+    // let array = Object.entries(data).map(function(element) {
+    //   if ((element[0] === "created_at") || (element[0] === "updated_at")) {
+    //     return returnDates(element);
+    //   } else if ((element[0] === "events"))  {
+    //     return returnEvents(element);
+    //   } else if (element[1] && (typeof element[1] === 'object'))  {
+    //     return returnObjects(element);
+    //   } else {
+    //     return returnRegular(element);
+    //   }
+    // });
+    // return array.join("");
+  } else {
+    return "This contact hasn't been created yet. Use the add button to create a new contact.";
+  }
+}
+
+function infoButton(name, contact_id, event_id){
+  return `<div id="small_button_${contact_id}" class="btn btn-primary btn-sm">${name}
+  <a href="#" class="more-button" data-id="${contact_id}">
+  <i class="material-icons inverse">info</i></a>
+  <a href="#" class="remove-button" data-contact-id="${contact_id}" data-event-id="${event_id}">
+  <i class="material-icons inverse">remove_circle</i></a> </div>`
+}
+
+// Attach Listeners Functions
+
 function attachInfoListeners(){
   $('.more-button').on("click", function(event) {
     event.preventDefault();
@@ -70,83 +137,3 @@ function attachRemoveListeners(){
       });
 
 }
-
-
-function humanDate(data){
-  let dateArray = data.split("T");
-  return dateArray[0];
-}
-
-function organizeInfo(data) {
-  if (data["id"]) {
-    let array = Object.entries(data).map(function(element) {
-      if ((element[0] === "created_at") || (element[0] === "updated_at")) {
-        return returnDates(element);
-      } else if ((element[0] === "events"))  {
-        return returnEvents(element);
-      } else if (element[1] && (typeof element[1] === 'object'))  {
-        return returnObjects(element);
-      } else {
-        return returnRegular(element);
-      }
-    });
-    return array.join("");
-  } else {
-    return "This contact hasn't been created yet. Use the add button to create a new contact.";
-  }
-}
-
-function infoButton(name, contact_id, event_id){
-  return `<div id="small_button_${contact_id}" class="btn btn-primary btn-sm">${name}
-  <a href="#" class="more-button" data-id="${contact_id}">
-  <i class="material-icons inverse">info</i></a>
-  <a href="#" class="remove-button" data-contact-id="${contact_id}" data-event-id="${event_id}">
-  <i class="material-icons inverse">remove_circle</i></a> </div>`
-}
-
-function returnDates(element){
-  return "<b>" + element[0] + ":</b> " + humanDate(element[1]) + "<br>";
-}
-
-function returnObjects(element){
-  return "<b>" + element[0] + ":</b> " + Object.values(element[1]) + "<br>";
-}
-
-function returnRegular(element){
-  return "<b>" + element[0] + ":</b> " + (element[1] || "N/A") + "<br>";
-}
-
-function returnEvents(element){
-  if (!!element[1][0]) {
-    return "<b>" + "last event attended:</b> " + Object.values(element[1][0])[1] + " " + Object.values(element[1][0])[2] + "<br>";
-  } else {
-    return "<b>" + "last event attended:</b> N/A <br>";
-  }
-
-}
-
-function grabId() {
-  let box = $('#event_contacts_name').val();
-  let id = $('#contacts_autocomplete option').filter(function() {
-      return this.value == box;
-  }).data('id');
-  return id;
-}
-
-function lookupInBox() {
-  $.get("/contacts/" + grabId() + ".json", function(data) {
-    let info = '<label class="lb-lg">More contact info</label><br>' + organizeInfo(data);
-    $('#more-div').html(info);
-  });
-}
-
-// function Contact(data) {
-//   this.name = data["name"];
-//   this.email = data["email"];
-//   this.gender = data["gender"];
-//   this.staff = data["user"]["name"];
-//   this.created = humanDate(data["created_at"])
-// }
-
-// Contact.prototype.humanDate = humanDate(data);
-// Event.prototype.humanDate = humanDate(data);
