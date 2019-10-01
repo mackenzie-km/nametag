c2fc1199569581d66f892c281f066ed2fda3ff82
github username <github email address>
2019-10-01 15:22:28 -0700

    fixed bug where you can't look up things after page has loaded

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,4 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :date, :created_at
+  attributes :id, :name, :date, :created_at, :staff_count, :guest_count
   has_many :contacts, serializer: ContactSerializer
 end
