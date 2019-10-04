2ad6abee34bba4b77209343f48ff79e2cf398b35
github username <github email address>
2019-10-03 21:32:14 -0700

    finalized javascript index page for contacts & refactored code

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -11,32 +11,21 @@ class Contact {
  constructor(data) {
     this.id = data["id"];
     this.name = data["name"]
-    this.email = data["email"];
-    this.major = data["major"];
-    this.staff = data["user"]["email"];
-    this.events = data["events"];
-    this.timestamp = data["created_at"];
+    this.email = data["email"] || "N/A";
+    this.staff_name = data["user"]["email"].split("@", 1)[0] || "N/A";
+    this.gender = data["gender"] || "N/A";
   };
 };
 
 
 // allows for contact.cleanTimestamp() method to standardize dates via the prototype
 
-Contact.prototype.cleanTimestamp = function() {
-  let dateArray = this.timestamp.split("T");
-  return dateArray[0];
-};
-
-
 function attachNameListener(){
  $('a#name').on("click", function(event) {
    event.preventDefault();
    if (!!$('input#contact_name').val()) {
      let criteria = $('input#contact_name').serialize()
-     $.getJSON("/contacts.json", criteria, function(data) {
-       debugger
-      }
-    );
+     getJSONRobot(criteria)
   }
  });
 }
@@ -46,31 +35,24 @@ function attachStaffListener(){
    event.preventDefault();
    if (!!$('input#contact_email').val()) {
      let criteria = $('input#contact_email').serialize()
-     $.getJSON("/contacts.json", criteria, function(data) {
-       debugger
-      }
-    );
-  }
- });
+     getJSONRobot(criteria)
+    }
+  });
 }
 
 function attachUnclaimedListener(){
  $('a#unclaimed').on("click", function(event) {
    event.preventDefault();
    let criteria = {"unclaimed": "true"}
-    $.getJSON("/contacts.json", criteria, function(data) {
-       debugger
-    });
- });
+   getJSONRobot(criteria)
+  });
 }
 
 function attachAllListener(){
  $('a#all').on("click", function(event) {
    event.preventDefault();
    let criteria = {"all": "true"}
-    $.getJSON("/contacts.json", criteria, function(data) {
-       debugger
-    });
+   getJSONRobot(criteria)
  });
 }
 
@@ -78,8 +60,35 @@ function attachRecentlyUpdatedListener(){
  $('a#recently_updated').on("click", function(event) {
    event.preventDefault();
    let criteria = {"recently_updated": "true"}
-    $.getJSON("/contacts.json", criteria, function(data) {
-       debugger
+   getJSONRobot(criteria)
+});
+}
+
+function getJSONRobot(criteria){
+  $('tbody').empty();
+  $.getJSON("/contacts.json", criteria, function(data) {
+    if (!(typeof data === Array)) { data = [].concat(data) }
+    data.map(function(object){
+     let contact = new Contact(object)
+     let html = tableHtmlPrinter(contact)
+     $('tbody').append(html)
     });
- });
+  });
+}
+
+function tableHtmlPrinter(contact){
+  return `<tr>
+    <td>${contact.name}</td>
+    <td>
+      <a href="/contacts/${contact.id}/edit">
+        <i class="material-icons">edit</i>
+      </a>
+      <a href="/contacts/${contact.id}">
+        <i class="material-icons">zoom_in</i>
+      </a>
+    </td>
+    <td>${contact.staff_name}</td>
+    <td>${contact.email}</td>
+    <td>${contact.gender}</td>
+  </tr>`
 }
